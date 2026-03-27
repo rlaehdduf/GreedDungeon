1d9ee245338c6a66774ee3f416701327ee161c24
dongyeul <rlaehdduf04@gmail.com>
2026-03-27 16:06:35 +0900

    feat: 창 최소 크기 제한 추가 (640x360)

--- Assets/Scripts/Core/WindowAspectRatio.cs
+++ Assets/Scripts/Core/WindowAspectRatio.cs
@@ -9,28 +9,28 @@ namespace GreedDungeon.Core
         [Header("Settings")]
         [SerializeField] private float _aspectRatio = 16f / 9f;
         [SerializeField] private bool _startFullscreen = true;
+        [SerializeField] private int _minWidth = 640;
         
 #if UNITY_STANDALONE_WIN
-        private delegate bool WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+        private delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
         
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern IntPtr SetWindowLong32(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
         
         [DllImport("user32.dll")]
         private static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
         
         [DllImport("user32.dll")]
-        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-        
-        [DllImport("user32.dll")]
-        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        private static extern IntPtr GetActiveWindow();
         
         [StructLayout(LayoutKind.Sequential)]
         private struct RECT { public int Left, Top, Right, Bottom; }
         
         private const int GWLP_WNDPROC = -4;
         private const uint WM_SIZING = 0x0214;
-        private const uint WM_EXITSIZEMOVE = 0x0232;
         
         private const int WMSZ_LEFT = 1;
         private const int WMSZ_RIGHT = 2;
@@ -41,19 +41,21 @@ namespace GreedDungeon.Core
         private const int WMSZ_BOTTOMLEFT = 7;
         private const int WMSZ_BOTTOMRIGHT = 8;
         
-        private const uint SWP_NOMOVE = 0x0002;
-        private const uint SWP_NOZORDER = 0x0004;
-        
         private static IntPtr _hWnd;
         private static IntPtr _oldWndProc;
         private static WndProcDelegate _wndProcDelegate;
         private static float _staticAspectRatio;
+        private static bool _isFullscreen;
+        private static int _staticMinWidth;
+        private static int _staticMinHeight;
         
         private void Awake()
         {
             _staticAspectRatio = _aspectRatio;
             _wndProcDelegate = WndProc;
-            
+            _isFullscreen = _startFullscreen;
+            _staticMinWidth = _minWidth;
+            _staticMinHeight = Mathf.RoundToInt(_minWidth / _aspectRatio);
             DontDestroyOnLoad(gameObject);
         }
         
@@ -65,21 +67,25 @@ namespace GreedDungeon.Core
             }
             else
             {
-                Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+                Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
             }
             
-#if UNITY_STANDALONE_WIN
             _hWnd = GetActiveWindow();
-            _oldWndProc = SetWindowLongPtr(_hWnd, GWLP_WNDPROC, Marshal.GetFunctionPointerForDelegate(_wndProcDelegate));
-#endif
+            
+            if (IntPtr.Size == 8)
+                _oldWndProc = SetWindowLongPtr64(_hWnd, GWLP_WNDPROC, Marshal.GetFunctionPointerForDelegate(_wndProcDelegate));
+            else
+                _oldWndProc = SetWindowLong32(_hWnd, GWLP_WNDPROC, Marshal.GetFunctionPointerForDelegate(_wndProcDelegate));
         }
         
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetActiveWindow();
-        
-        private static bool WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        private void Update()
         {
-            if (msg == WM_SIZING && Screen.fullScreenMode == FullScreenMode.Windowed)
+            _isFullscreen = Screen.fullScreen;
+        }
+        
+        private static IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            if (msg == WM_SIZING && !_isFullscreen)
             {
                 int edge = wParam.ToInt32();
                 RECT rect = (RECT)Marshal.PtrToStructure(lParam, typeof(RECT));
@@ -87,60 +93,89 @@ namespace GreedDungeon.Core
                 int width = rect.Right - rect.Left;
                 int height = rect.Bottom - rect.Top;
                 
-                int newWidth;
-                int newHeight;
+                // 최소 크기 체크
+                if (width < _staticMinWidth) width = _staticMinWidth;
+                if (height < _staticMinHeight) height = _staticMinHeight;
                 
                 switch (edge)
                 {
                     case WMSZ_LEFT:
+                        {
+                            int newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
+                            rect.Bottom = rect.Top + newHeight;
+                            rect.Left = rect.Right - width;
+                            break;
+                        }
                     case WMSZ_RIGHT:
-                        newWidth = width;
-                        newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
-                        rect.Bottom = rect.Top + newHeight;
-                        break;
+                        {
+                            int newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
+                            rect.Bottom = rect.Top + newHeight;
+                            rect.Right = rect.Left + width;
+                            break;
+                        }
                         
                     case WMSZ_TOP:
+                        {
+                            int newWidth = Mathf.RoundToInt(height * _staticAspectRatio);
+                            rect.Right = rect.Left + newWidth;
+                            rect.Top = rect.Bottom - height;
+                            break;
+                        }
                     case WMSZ_BOTTOM:
-                        newHeight = height;
-                        newWidth = Mathf.RoundToInt(height * _staticAspectRatio);
-                        rect.Right = rect.Left + newWidth;
-                        break;
+                        {
+                            int newWidth = Mathf.RoundToInt(height * _staticAspectRatio);
+                            rect.Right = rect.Left + newWidth;
+                            rect.Bottom = rect.Top + height;
+                            break;
+                        }
                         
                     case WMSZ_TOPLEFT:
-                        newWidth = width;
-                        newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
-                        rect.Top = rect.Bottom - newHeight;
-                        break;
-                        
+                        {
+                            int newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
+                            rect.Top = rect.Bottom - newHeight;
+                            rect.Left = rect.Right - width;
+                            break;
+                        }
                     case WMSZ_TOPRIGHT:
-                        newWidth = width;
-                        newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
-                        rect.Top = rect.Bottom - newHeight;
-                        break;
+                        {
+                            int newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
+                            rect.Top = rect.Bottom - newHeight;
+                            rect.Right = rect.Left + width;
+                            break;
+                        }
                         
                     case WMSZ_BOTTOMLEFT:
+                        {
+                            int newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
+                            rect.Bottom = rect.Top + newHeight;
+                            rect.Left = rect.Right - width;
+                            break;
+                        }
                     case WMSZ_BOTTOMRIGHT:
-                        newWidth = width;
-                        newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
-                        rect.Bottom = rect.Top + newHeight;
-                        break;
+                        {
+                            int newHeight = Mathf.RoundToInt(width / _staticAspectRatio);
+                            rect.Bottom = rect.Top + newHeight;
+                            rect.Right = rect.Left + width;
+                            break;
+                        }
                 }
                 
                 Marshal.StructureToPtr(rect, lParam, true);
-                return true;
+                return IntPtr.Zero;
             }
             
-            return CallWindowProc(_oldWndProc, hWnd, msg, wParam, lParam) != IntPtr.Zero;
+            return CallWindowProc(_oldWndProc, hWnd, msg, wParam, lParam);
         }
         
         private void OnDestroy()
         {
-#if UNITY_STANDALONE_WIN
             if (_hWnd != IntPtr.Zero && _oldWndProc != IntPtr.Zero)
             {
-                SetWindowLongPtr(_hWnd, GWLP_WNDPROC, _oldWndProc);
+                if (IntPtr.Size == 8)
+                    SetWindowLongPtr64(_hWnd, GWLP_WNDPROC, _oldWndProc);
+                else
+                    SetWindowLong32(_hWnd, GWLP_WNDPROC, _oldWndProc);
             }
-#endif
         }
 #endif
     }
